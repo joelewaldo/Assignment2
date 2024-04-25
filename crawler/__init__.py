d4347e631b6b0ff3da6d3547110679c426b4ea67
Ostend Dylan Suryajaya <osuryaja@circinus-21.ics.uci.edu>
2024-04-24 20:19:08 -0700

    Initialized checksums in __init__

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,14 +1,16 @@
 from utils import get_logger
 from crawler.frontier import Frontier
 from crawler.worker import Worker
+from crawler.checksums import Checksums
 
 class Crawler(object):
-    def __init__(self, config, restart, frontier_factory=Frontier, worker_factory=Worker):
+    def __init__(self, config, restart, frontier_factory=Frontier, worker_factory=Worker, checksums_factory=Checksums):
         self.config = config
         self.logger = get_logger("CRAWLER")
         self.frontier = frontier_factory(config, restart)
         self.workers = list()
         self.worker_factory = worker_factory
+        self.checksums = checksums_factory(config, restart)
 
     def start_async(self):
         self.workers = [
