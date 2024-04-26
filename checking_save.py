27374943b6d7d80a59b7c88211d389a89e74f24d
Joel Ewaldo <jewaldo@uci.edu>
2024-04-26 11:55:03 -0700

    Fixing last minute bugs

--- checking_save.py
+++ checking_save.py
@@ -71,9 +71,9 @@ class SaveChecker:
 
         for url in urls:
             parsed_url = urlparse(url)
-            if parsed_url.netloc.endswith("ics.uci.edu"):
+            if parsed_url.netloc.endswith(".ics.uci.edu"):
                 # Extract the subdomain part and normalize to lowercase
-                subdomain = parsed_url.netloc.lower()
+                subdomain = parsed_url.netloc.lower().strip()
                 # Use a set for the path to ensure uniqueness
                 subdomains[subdomain].add(parsed_url.path)
 
